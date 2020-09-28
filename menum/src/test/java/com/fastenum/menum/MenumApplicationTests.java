d88ad71115633a62cf04b9b566736d83afc95380
zhangming <zhangming1@aixuexi.com>
2020-09-28 11:29:31 +0800

    项目 - 增加管理模块

---
+++ menum/src/test/java/com/fastenum/menum/MenumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastenum.menum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
