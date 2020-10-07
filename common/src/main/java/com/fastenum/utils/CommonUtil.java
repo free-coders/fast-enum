151943677179b68e0b76e5eea9ca50800a18fd1b
zhangming <zhangming1@aixuexi.com>
2020-10-07 09:47:56 +0800

    fmenum 增加admin登陆.重构。

--- common/src/main/java/com/fastenum/utils/CommonUtil.java
+++ common/src/main/java/com/fastenum/utils/CommonUtil.java
@@ -1,13 +1,20 @@
 package com.fastenum.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @Description : 工具类
  * @Author : zhangMing
  * @Date : Created in 18:41 2020-09-27
  */
+@Slf4j
 public class CommonUtil {
 
     /**
@@ -24,6 +31,27 @@ public class CommonUtil {
             stringBuilder.append( fieldError.getDefaultMessage() );
         }
         return stringBuilder.subSequence( 0 , stringBuilder.length() - 1 ).toString();
+    }
+
+
+    /**
+     * md5加密
+     * @param str
+     * @return
+     */
+    public static String encodeByMd5(String str) {
+        String result = "";
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance( "MD5" );
+            BASE64Encoder base64Encoder = new BASE64Encoder();
+            result = base64Encoder.encode( messageDigest.digest( str.getBytes("utf-8")) );
+        } catch (NoSuchAlgorithmException e) {
+            //获取DM5示例失败
+            log.info( "获取DM5示例失败" );
+        } catch (UnsupportedEncodingException e) {
+            log.info( "转换utf-8字节失败" );
+        }
+        return result;
     }
 
 }
--- menum/src/main/java/com/fastenum/menum/controller/AdminController.java
+++ menum/src/main/java/com/fastenum/menum/controller/AdminController.java
@@ -2,20 +2,14 @@ package com.fastenum.menum.controller;
 
 import com.fastenum.bo.EnumBO;
 import com.fastenum.exception.BusinessException;
+import com.fastenum.utils.CommonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import sun.misc.BASE64Encoder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * @Description : 登陆控制层
@@ -39,7 +33,13 @@ public class AdminController {
     private String encryptPassword;
 
 
-    @RequestMapping(value="/login" , method=RequestMethod.POST)
+    /**
+     * 登陆接口
+     * @param email
+     * @param password
+     * @return
+     */
+    @PostMapping( "/login" )
     public String loginpage(@RequestParam( name="email" ) String email ,
                             @RequestParam( name="password") String password) {
         if (StringUtils.isEmpty(email) || StringUtils.isEmpty( password )){
@@ -47,13 +47,13 @@ public class AdminController {
             EnumBO errorEnumBo = new EnumBO();
             throw new BusinessException( errorEnumBo );
         }
-        if (email.equals( this.email ) && encodeByMd5( password ).equals( this.encryptPassword )){
+        if (email.equals( this.email ) && CommonUtil.encodeByMd5( password ).equals( this.encryptPassword )){
             //登陆成功,admin登陆设置全部权限
             httpServletRequest.getSession().setAttribute( CURRENT_ADMIN_SESSION , email );
             //跳转到首页
             return "redirect:/admin/admin/index";
         }else{
-            log.info( "密码为:{}" , encodeByMd5( password ) );
+            log.info( "密码为:{}" , CommonUtil.encodeByMd5( password ) );
             //登陆失败 用户名或密码不能为空
             EnumBO errorEnumBo = new EnumBO();
             throw new BusinessException( errorEnumBo );
@@ -61,25 +61,7 @@ public class AdminController {
 
     }
 
-    /**
-     * md5加密
-     * @param str
-     * @return
-     */
-    private String encodeByMd5(String str) {
-        String result = "";
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance( "MD5" );
-            BASE64Encoder base64Encoder = new BASE64Encoder();
-            result = base64Encoder.encode( messageDigest.digest( str.getBytes("utf-8")) );
-        } catch (NoSuchAlgorithmException e) {
-            //获取DM5示例失败
-            log.info( "获取DM5示例失败" );
-        } catch (UnsupportedEncodingException e) {
-            log.info( "转换utf-8字节失败" );
-        }
-        return result;
-    }
+
 }
 
 
